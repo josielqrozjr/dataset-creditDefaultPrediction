3c32bb9fda00d84b8f3df14ae8db549515edd9ca
Queiroz JR <131998280+josielqrozjr@users.noreply.github.com>
2026-05-14 21:08:32 -0300

    fix: update comment to clarify parquet file path in getSchema.py

--- schemas/getSchema.py
+++ schemas/getSchema.py
@@ -5,7 +5,7 @@ con = duckdb.connect()
 schema = con.execute("""
 DESCRIBE
 SELECT *
-FROM 'data/raw/parquet/train/*0.parquet';
+FROM 'data/raw/parquet/train/*0.parquet'; # Ajuste o caminho para um dos arquivos Parquet gerados
 """).fetchdf()
 
 schema.to_csv("schemas/schema.csv", index=False)
