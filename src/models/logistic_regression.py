5c66b75a04c8034d5a68d3b3e55af07dde18b65e
queirozjr <josiel.junior@ebanx.com>
2026-05-31 18:38:14 -0300

    refactor: remove explicit random_state from LogisticRegression instantiation

--- src/models/logistic_regression.py
+++ src/models/logistic_regression.py
@@ -1,9 +1,10 @@
 """Regressão Logística."""
 
 from sklearn.linear_model import LogisticRegression
-from config import RANDOM_SEED, HYPERPARAMS
+from config import HYPERPARAMS
 
 
 def build_model():
     params = HYPERPARAMS["Logistic Regression"]
-    return LogisticRegression(random_state=RANDOM_SEED, **params)
+    # Passamos apenas o **params, pois o config já tem o random_state!
+    return LogisticRegression(**params)
